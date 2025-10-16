ee6b8b7edfb292aade8a5e54a8413a0c0924e77e
Ganza ngiruwonsanga aldo <aldongiruwonsanga@gmail.com>
2025-10-16 22:01:11 +0200

    thousday commit

---
+++ classgram/frontend/src/pages/Settings.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Settings = () => {
+  return (
+    <div className="settings-page">
+      <h1>Settings</h1>
+      <p>Update your account preferences here.</p>
+      {/* Add settings form or options here */}
+    </div>
+  );
+};
+
+export default Settings;
